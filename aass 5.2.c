e9d950ffe849f6ee464167b6d0309350325d6771
mrshankark059-byte <mrshankark059@gmail.com>
2026-02-26 19:30:20 +0530

    Create aass 5.2.c

---
+++ aass 5.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int x;
+ printf("Enter a numbers");
+ scanf("%d",&x);
+ if(x%5==0)      
+ {
+ printf("Yes"); 
+ }
+ else  
+ {
+  printf("No"); 
+ } 
+    return 0;
+}
+    
