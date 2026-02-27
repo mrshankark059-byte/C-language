54a154f4a8169c3f9d7e19254cfd8581764d1faf
mrshankark059-byte <mrshankark059@gmail.com>
2026-02-28 00:29:47 +0530

    Create Assignment 5.4.c

---
+++ Assignment 5.4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Enter a number");
+  scanf("%d",&a);
+  if(a/2*2==a) 
+  {
+   printf("Even number");
+  }
+  else
+  {
+   printf("odd number");
+  }
+    return 0;
+}   
