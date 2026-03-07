634e50faa06d8e6270b5d2d63af7a05f209fb88f
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-07 08:09:54 +0530

    Create Assignment 7.3.C

---
+++ Assignment 7.3.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{ 
+  int a=10;
+  while (a>=1)
+  {
+   printf("%d\n",a);
+   a--;
+  }
+    return 0;
+}
+
+
+
+Runnnnn
+
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+[Process completed - press Enter]
